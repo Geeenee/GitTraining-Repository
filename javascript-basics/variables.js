8488386ed0b7a24888f2293ac4d7ca8a474a27f1
Gene <gvasoriano257@gmail.com>
2026-02-03 08:08:55 +0800

    Exercise with variables - naming convention, const and let declaration

--- javascript-basics/variables.js
+++ javascript-basics/variables.js
@@ -1,1 +1,12 @@
-console.log('Hello World! - From VIM')
+let admin
+let name
+
+name = "John"
+admin = name
+
+console.log(admin)
+
+let ourPlanet, currentUserName
+
+const birthday = new Date()
+const age = 0

5f69cd34a9a43b4ae7b46b8b23d807c64ff10af5
Gene <gvasoriano257@gmail.com>
2026-02-09 15:16:41 +0800

    Added additional code samples

--- javascript-basics/for-loop-demo.js
+++ javascript-basics/for-loop-demo.js
@@ -1,17 +1,23 @@
-//for(let i = 2; i <= 10; i++){
-//	if(i % 2 == 1) continue;
-//	console.log(i)
-//};
-//
-//
+
+let n = 10
+outerLoop: for(let i = 2; i <= n; i++){
+	for(let j = 2; j < i; j++ ){
+		
+		if(i % j == 0) continue outerLoop;
+		
+
+	}	
+	console.log(i);
+};
+
 //let i = 0
 //while(i < 3){
 //	console.log(`number ${i}!`)
 //	i++
 //}
-let answer
-while(answer <= 100 || answer === null){
-	prompt('Input a number greater than one hundred', null)
-	
-}
+//let answer
+//while(answer <= 100 || answer === null){
+//	prompt('Input a number greater than one hundred', null)
+//	
+//}
 

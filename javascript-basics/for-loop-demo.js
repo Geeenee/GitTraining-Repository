d6b1bb9322f2203da0ff84a3cb490edfb1131328
Gene <gvasoriano257@gmail.com>
2026-02-09 10:43:52 +0800

    Added code sample for for-loop tasks

---
+++ javascript-basics/for-loop-demo.js
@@ -0,0 +1,17 @@
+//for(let i = 2; i <= 10; i++){
+//	if(i % 2 == 1) continue;
+//	console.log(i)
+//};
+//
+//
+//let i = 0
+//while(i < 3){
+//	console.log(`number ${i}!`)
+//	i++
+//}
+let answer
+while(answer <= 100 || answer === null){
+	prompt('Input a number greater than one hundred', null)
+	
+}
+

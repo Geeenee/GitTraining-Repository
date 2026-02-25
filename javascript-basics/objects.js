1b864281c685c44fa60e5081a9cb9c8256503f27
Gene <gvasoriano257@gmail.com>
2026-02-25 11:05:13 +0800

    Added a script for object related js tasks

---
+++ javascript-basics/objects.js
@@ -0,0 +1,16 @@
+let user = {}
+user['name'] = 'John'
+user['surname'] = 'Smith'
+
+console.log(user.name + user.surname)
+
+user['name'] = 'Pete'
+
+console.log(user.name + user.surname)
+
+delete user['name']
+
+console.log(user.name + user.surname)
+
+
+

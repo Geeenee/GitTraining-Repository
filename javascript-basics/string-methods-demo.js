78b1cc9cb6bcf308ba50247c80564a46bbff16f2
Gene <gvasoriano257@gmail.com>
2026-03-04 10:41:19 +0800

    Added word filter via string methods sample

--- javascript-basics/string-methods-demo.js
+++ javascript-basics/string-methods-demo.js
@@ -1,3 +1,12 @@
+let checkSpam = (word) => {
+	word = word.toLowerCase();
+	if(word.includes('viagra') || word.includes('xxx')) return true;
+	return false;
+};
+console.log(checkSpam(`buy ViAgRA now`));
+console.log(checkSpam(`free xxxxx`));
+console.log(checkSpam(`innocent rabbit`));
+return;
 let ucFirst = (word) => {
 	if ( !str ) return;
 

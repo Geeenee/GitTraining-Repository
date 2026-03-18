b94924c2e85a54c6d0e8abf817a8ca946fdac394
Gene <gvasoriano257@gmail.com>
2026-03-18 15:37:52 +0800

    Added string to camelCase format converter sample

--- javascript-basics/arrays/array-demo.js
+++ javascript-basics/arrays/array-demo.js
@@ -1,3 +1,16 @@
+
+let camelize = (str) => {
+	let splitStr = str.split('-');
+	let camelCaseStr = splitStr.map( (item, index) => index === 0 ? item : item[0].toUpperCase() + item.slice(1)).join('');
+	console.log(camelCaseStr);
+
+
+};
+
+camelize("background-color");
+camelize("list-style-image");
+camelize("-webkit-transition");
+return;
 let getArrayCenter = function(array){
 	return Math.round( (((array.length -1) / 2) * 100) / 100 );
 };

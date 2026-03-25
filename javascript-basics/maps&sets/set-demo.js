171a12b2dedc916a9e8c01596fa3b88a6cd08e14
Gene <gvasoriano257@gmail.com>
2026-03-25 16:49:19 +0800

    Refactored code sample

--- javascript-basics/maps&sets/set-demo.js
+++ javascript-basics/maps&sets/set-demo.js
@@ -1,15 +1,5 @@
 function unique(arr) {
-	let dataSet = new Set()
-  	for (let value of arr){
-		dataSet.add(value);
-		//console.log(value);
-  	} 
-	let arry = []
-	for (let value of dataSet){
-		arry.push(value);
-	}
-	return arry;
-	
+	return Array.from(new Set(arr));	
 }
 
 let values = ["Hare", "Krishna", "Hare", "Krishna",

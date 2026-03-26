b4cae7dea392b29f8aeb49906402245aff4a0a37
Gene <gvasoriano257@gmail.com>
2026-03-26 08:28:21 +0800

    Added 'usage of Array.from' code sample

--- javascript-basics/maps&sets/set-demo.js
+++ javascript-basics/maps&sets/set-demo.js
@@ -1,3 +1,14 @@
+let map = new Map();
+
+map.set("name", "John");
+
+let keys = Array.from(map.keys());
+
+// Error: keys.push is not a function
+keys.push("more");
+
+console.log(keys);
+return;
 function unique(arr) {
 	return Array.from(new Set(arr));	
 }

a365ead9c2430ec49a2b8cf7a72a59a8adbfaa3e
Gene <gvasoriano257@gmail.com>
2026-04-01 14:48:03 +0800

    Added 'return name of employee with highest salary' code sample

--- javascript-basics/objects/destructuring-objects.js
+++ javascript-basics/objects/destructuring-objects.js
@@ -1,7 +1,21 @@
+let salaries = {
+	'Joe' : 65,
+	'John': 50,
+	'Doe' : 23,
+}
+
+  let topSalary = function(obj){
+    let entries = Object.entries(obj);
+    return entries.length ? entries.sort( (a, b) => b[1] - a[1])[0][0] : null;
+  }
+
+console.log(topSalary(salaries));
+return;
 let user = {
-	name : 'Gene',
-	years : 25,
+	name : 'John',
+	years : 30
 };
+
 
 let name, age, isAdmin;
 

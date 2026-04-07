7cfa60ddd4276e9344d258fc53b942d9130cb593
Gene <gvasoriano257@gmail.com>
2026-04-07 14:36:30 +0800

    Added 'get last day of the given month of a year' code sample

--- javascript-basics/date/dates-demo.js
+++ javascript-basics/date/dates-demo.js
@@ -1,4 +1,11 @@
-  let getDateAgo = (date, day) => {
+  let getLastDayOfMonth = (year, month) => {
+    return new Date(year, month + 1, 0).getDate();
+  }
+console.log(getLastDayOfMonth(2012, 1));
+return;
+
+
+let getDateAgo = (date, day) => {
     return new Date(date.getFullYear(), date.getMonth(), date.getDate() - day ).getDate();
   }
 

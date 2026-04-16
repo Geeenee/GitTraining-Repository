6bae4898ac71f5caa181ec0729af5ec3bade0abb
Gene <gvasoriano257@gmail.com>
2026-04-16 16:08:02 +0800

    Added 'exclude property with circle reference from object' code sample

--- javascript-basics/JSON/JSON-demo.js
+++ javascript-basics/JSON/JSON-demo.js
@@ -1,8 +1,28 @@
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  occupiedBy: [{name: "John"}, {name: "Alice"}],
+  place: room
+};
+
+// circular references
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+
+console.log( JSON.stringify(meetup, function replacer(key, value) {
+	if( !key ) return value; 
+	return value === meetup ? undefined : value;
+}));
+return;
+
 let user = {
 	name: "John Smith",
 	age: 35,
 };
-
 let userJson = JSON.stringify(user)
 
 console.log(JSON.parse(userJson));

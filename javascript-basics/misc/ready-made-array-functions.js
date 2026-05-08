a277d3d7f882e2ac3d60b8b46e98af784c1e5add
Gene <gvasoriano257@gmail.com>
2026-05-08 17:17:59 +0800

    Added 'extra ready made filter function - key as argument for sorting' code sample

--- javascript-basics/misc/ready-made-array-functions.js
+++ javascript-basics/misc/ready-made-array-functions.js
@@ -1,4 +1,19 @@
-  let arr = [1, 2, 3, 4, 5, 6, 7];
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+let byField = (field) => {
+	return function(a, b){
+		return a[field] > b[field] ? 1 : -1
+	};	
+};
+
+console.log(users.sort(byField('age')));
+return;
+{
+let arr = [1, 2, 3, 4, 5, 6, 7];
   
   function inBetween(a, b) {
     return function(data){
@@ -16,3 +31,4 @@
 
 //console.log(arr.filter(inArray([1, 2 ,3, 10])));
 console.log(arr.filter(inBetween(2, 4)));
+}

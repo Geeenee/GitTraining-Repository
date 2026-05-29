f9eccf120180a69c42fd83bd673b42553fe406e6
Gene <gvasoriano257@gmail.com>
2026-05-29 16:02:40 +0800

    Re-aligned nested setTimeout code sample based on task specs

--- javascript-basics/time&interval/timeout&interval.js
+++ javascript-basics/time&interval/timeout&interval.js
@@ -1,10 +1,10 @@
 function printNumbers(from, to){ //nested setTimeout
-	if(from > to) return;
-
-	console.log(from);
-
-	setTimeout(printNumbers, 1000, from + 1, to)
+	let current = from;
+	let timeoutId = setTimeout(function printNum(){ console.log(current); if(from == to) clearTimeout(timeoutId); current++; setTimeout(printNum(), 1000)}, 1000)
 };
+
+printNumbers(1, 5);
+return;
 
 function printNumbers(from, to){ //setInterval
 	let printInterval = setInterval(() => {console.log(from); if(from == to) clearInterval(printInterval); from++; }, 1000)

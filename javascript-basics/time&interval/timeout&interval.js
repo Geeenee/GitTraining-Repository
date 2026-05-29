b32888c3a1a01290c37caa1d208a88cc3be2d981
Gene <gvasoriano257@gmail.com>
2026-05-29 15:45:28 +0800

    Added 'print numbers from n to y via setInterval' code sample

--- javascript-basics/time&interval/timeout&interval.js
+++ javascript-basics/time&interval/timeout&interval.js
@@ -6,4 +6,8 @@ function printNumbers(from, to){ //nested setTimeout
 	setTimeout(printNumbers, 1000, from + 1, to)
 };
 
+function printNumbers(from, to){ //setInterval
+	let printInterval = setInterval(() => {console.log(from); if(from == to) clearInterval(printInterval); from++; }, 1000)
+};
+
 printNumbers(1, 5)

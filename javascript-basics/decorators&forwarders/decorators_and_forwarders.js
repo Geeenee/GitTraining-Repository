2dc197bd017e9fe9c9debc82bde7280494478948
Gene <gvasoriano257@gmail.com>
2026-06-25 13:45:37 +0800

    Added 'leading and trailing edge throttling decorator' code sample

--- javascript-basics/decorators&forwarders/decorators_and_forwarders.js
+++ javascript-basics/decorators&forwarders/decorators_and_forwarders.js
@@ -23,3 +23,31 @@ let debounce = function(fn, ms){
 
 	}
 }
+
+
+  function throttle(fn, ms){
+
+    let currThis, currArgs, block = false;
+
+    return function wrapper(){
+
+      if(block){
+      currThis = this;
+      currArgs = arguments;
+      return;
+      }
+
+        fn.apply(this, arguments);
+        block = true;
+
+        setTimeout(() => {
+          block = false
+            if(currArgs){
+              wrapper.apply(currThis, currArgs);
+              currThis, currArgs = null;
+            }
+          }, ms)
+
+    }
+
+  }

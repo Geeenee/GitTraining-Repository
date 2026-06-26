3f95de63b6f01952ae6a17bd9413f1a22116f0b6
Gene <gvasoriano257@gmail.com>
2026-06-26 16:46:27 +0800

    Added another 'function binding' code sample

--- javascript-basics/function-binding/function-binding.js
+++ javascript-basics/function-binding/function-binding.js
@@ -18,3 +18,19 @@ let user = {
 };
 
 askPassword(user.loginOk.bind(user), user.loginFail.bind(user);
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: 'John',
+
+  login(result) {
+    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+  }
+};
+
+askPassword(user.login.bind(user, true), () => user.login(false)); // ?

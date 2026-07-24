0430d90c15eb9c44d26ac7b5952ec5f5c0960903
Gene <gvasoriano257@gmail.com>
2026-07-24 16:32:42 +0800

    Added extra 'prototype' code sample

--- javascript-basics/prototypes/prototypes.js
+++ javascript-basics/prototypes/prototypes.js
@@ -24,4 +24,25 @@ let start = performance.now()
 console.log(pockets.glasses);
 console.log(performance.now() - start);
 
+let hamster = {
+  stomach: [],
 
+  eat(food) {
+    this.stomach = [food];
+  }
+};
+
+let speedy = {
+  __proto__: hamster
+};
+
+let lazy = {
+  __proto__: hamster
+};
+
+// This one found the food
+speedy.eat("apple");
+console.log( speedy.stomach ); // apple
+
+// This one also has it, why? fix please.
+console.log( lazy.stomach ); // apple
